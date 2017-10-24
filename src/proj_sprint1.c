702172e4234626bd975b124a1036e1a842a42cd4
Sirbu Piotr <33027405+SirbuPiotr@users.noreply.github.com>
2017-10-24 17:37:15 +0300

    tc

--- src/proj_sprint1.c
+++ src/proj_sprint1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World second Git Commit!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World total comander!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
