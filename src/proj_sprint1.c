5de8846e0ea02f2b7ef1ac8bf29ead752203f607
Sirbu Piotr <Sirbu Piotr@192.168.82.248>
2017-10-24 15:19:50 +0300

    commit second

--- src/proj_sprint1.c
+++ src/proj_sprint1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World first Git Commit!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World second Git Commit!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
