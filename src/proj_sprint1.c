0f20819576b39c971e9de8de3aae60730109afc4
Sirbu Piotr <33027405+SirbuPiotr@users.noreply.github.com>
2017-10-24 17:44:09 +0300

    total

--- src/proj_sprint1.c
+++ src/proj_sprint1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World total comander!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World total "); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
